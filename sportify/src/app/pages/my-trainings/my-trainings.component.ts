a6c1e38e07e8d28efe5d36572dea5cf69665afc8
Axelmanza <manzanares712@gmail.com>
2020-08-11 00:05:27 +0200

    landing and coach menu

---
+++ sportify/src/app/pages/my-trainings/my-trainings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-trainings',
+  templateUrl: './my-trainings.component.html',
+  styleUrls: ['./my-trainings.component.css']
+})
+export class MyTrainingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
